05d08489a012c068330907ea2fe42443f628b2c6
Cedric Mailleux <cedric.mailleux@jahia.com>
2013-05-24 11:51:59 -0400

    manage removal of tags special case for empty tags (add new, remove all)
    Inject TaggingService

--- src/main/java/org/jahia/modules/forge/actions/EditTags.java
+++ src/main/java/org/jahia/modules/forge/actions/EditTags.java
@@ -1,24 +1,22 @@
 package org.jahia.modules.forge.actions;
 
-import org.apache.jackrabbit.value.ReferenceValue;
-import org.apache.jackrabbit.value.WeakReferenceValue;
 import org.jahia.bin.Action;
 import org.jahia.bin.ActionResult;
 import org.jahia.services.content.JCRNodeWrapper;
 import org.jahia.services.content.JCRPropertyWrapper;
 import org.jahia.services.content.JCRSessionWrapper;
-import org.jahia.services.content.nodetypes.ValueImpl;
 import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.Resource;
 import org.jahia.services.render.URLResolver;
-import org.jahia.services.search.jcr.JahiaJCRSearchProvider;
 import org.jahia.services.tags.TaggingService;
 import org.slf4j.Logger;
 
-import javax.jcr.PropertyType;
 import javax.jcr.Value;
 import javax.servlet.http.HttpServletRequest;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Date: 2013-05-23
@@ -29,6 +27,7 @@ import java.util.*;
 public class EditTags extends Action {
 
     private transient static Logger logger = org.slf4j.LoggerFactory.getLogger(EditTags.class);
+    private TaggingService taggingService;
 
     @Override
     public ActionResult doExecute(HttpServletRequest req, RenderContext renderContext, Resource resource,
@@ -39,39 +38,54 @@ public class EditTags extends Action {
         String title = module.getPropertyAsString("jcr:title");
 
         session.checkout(module);
-
+        String siteKey = renderContext.getSite().getSiteKey();
         logger.info("Start editing tags of Forge module " + title + " " + module.getPath());
+        if (module.hasProperty("j:tags")) {
+            JCRPropertyWrapper tags = module.getProperty("j:tags");
+            Value[] values = tags.getValues();
 
-        JCRPropertyWrapper tags = module.getProperty("j:tags");
-        Value[] values = tags.getValues();
+            ArrayList<String> submittedTags = (ArrayList<String>) parameters.get(
+                    "tags-" + module.getIdentifier() + "[]");
+            if (submittedTags == null || submittedTags.isEmpty()) {
+                tags.remove();
+            } else {
+                ArrayList<String> currentTags = new ArrayList<String>();
+                Map<String, Value> valueMap = new HashMap<String, Value>();
+                for (Value tag : values) {
+                    String name = session.getNodeByIdentifier(tag.getString()).getName();
+                    currentTags.add(name);
+                    valueMap.put(name, tag);
+                }
 
-        ArrayList<String> submittedTags = (ArrayList<String>) parameters.get("tags-" + module.getIdentifier() + "[]");
+                ArrayList<String> removedTags = new ArrayList<String>(currentTags);
+                removedTags.removeAll(submittedTags);
 
-        ListIterator<String> listIterator = submittedTags.listIterator();
-        while(listIterator.hasNext())
-            listIterator.set(listIterator.next().toLowerCase());
+                for (String tag : removedTags) {
+                    tags.removeValue(valueMap.get(tag));
+                }
+                //tags.removeValue(new WeakReferenceValue( taggingService.getTag(tag, renderContext.getSite().getSiteKey(), session)));
 
-        ArrayList<String> currentTags = new ArrayList<String>();
-        for (Value tag : values)
-            currentTags.add(session.getNodeByIdentifier(tag.getString()).getName());
-
-        TaggingService taggingService = new TaggingService();
-
-        ArrayList<String> removedTags = new ArrayList<String>(currentTags);
-        removedTags.removeAll(submittedTags);
-        for (String tag : removedTags)
-            tags.removeValue(new ValueImpl(taggingService.getTag(tag, renderContext.getSite().getSiteKey(), session).getIdentifier(), PropertyType.REFERENCE));
-            //tags.removeValue(new WeakReferenceValue( taggingService.getTag(tag, renderContext.getSite().getSiteKey(), session)));
-
-        ArrayList<String> newTags = new ArrayList<String>(submittedTags);
-        if(newTags.removeAll(currentTags))
-            for (String tag : newTags)
-                taggingService.tag(module, tag, renderContext.getSite().getSiteKey(), true);
-
+                ArrayList<String> newTags = new ArrayList<String>(submittedTags);
+                if (newTags.removeAll(currentTags)) {
+                    for (String tag : newTags) {
+                        taggingService.tag(module, tag, siteKey, true);
+                    }
+                }
+            }
+        } else {
+            ArrayList<String> submittedTags = (ArrayList<String>) parameters.get(
+                    "tags-" + module.getIdentifier() + "[]");
+            for (String submittedTag : submittedTags) {
+                taggingService.tag(module, submittedTag, siteKey, true);
+            }
+        }
         session.save();
-
         logger.info("Tags of Forge module " + title + " successfully edited " + module.getPath());
 
         return ActionResult.OK_JSON;
+    }
+
+    public void setTaggingService(TaggingService taggingService) {
+        this.taggingService = taggingService;
     }
 }
