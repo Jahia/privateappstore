fcc2215febbfa320525e15b3febc36117cc4d41a
dgriffon <dgriffon@jahia.com>
2013-09-24 16:29:20 +0200

    BACKLOG-4241 : redirect to / when logout from non published module

--- src/main/java/org/jahia/modules/forge/filters/PublishedModuleFilter.java
+++ src/main/java/org/jahia/modules/forge/filters/PublishedModuleFilter.java
@@ -12,7 +12,8 @@ public class PublishedModuleFilter extends AbstractFilter {
     @Override
     public String prepare(RenderContext renderContext, Resource resource, RenderChain chain) throws Exception {
         if (!resource.getNode().getProperty("published").getBoolean() && !resource.getNode().hasPermission("jcr:write")) {
-            throw new PathNotFoundException("module is not published");
+            renderContext.getResponse().sendRedirect(renderContext.getResponse().encodeRedirectURL(renderContext.getRequest().getContextPath() + "/"));
+            return null;
         }
         return super.prepare(renderContext, resource, chain);
     }
