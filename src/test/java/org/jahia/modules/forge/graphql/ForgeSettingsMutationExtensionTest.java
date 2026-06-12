da01e75fa104caa1cfa71db074c08f38f53c3694
Florent BOURASSE <fbourasse@jahia.com>
2026-06-12 15:57:11 +0200

    test: drop unused JUnit Test import in ForgeSettingsMutationExtensionTest
    The class uses only @ParameterizedTest, so the bare org.junit.jupiter.api.Test
    import was dead (S1128). Leftover from the blind-review JUnit layer.
    
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- src/test/java/org/jahia/modules/forge/graphql/ForgeSettingsMutationExtensionTest.java
+++ src/test/java/org/jahia/modules/forge/graphql/ForgeSettingsMutationExtensionTest.java
@@ -1,7 +1,6 @@
 package org.jahia.modules.forge.graphql;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
